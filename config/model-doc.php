507fade721d7c139e45fca80474d6a7133e3eddc
Roman Zipp <ich@ich.wtf>
2021-10-06 09:18:32 +0200

    Update config docs

--- config/model-doc.php
+++ config/model-doc.php
@@ -3,7 +3,7 @@
 return [
     'relations' => [
         // Enable generating PHPDoc properties for relation methods
-        // @property MyRelatedModel|null $my_relation
+        // @property MyRelatedModel|null $myRelation
         'enabled' => true,
 
         'counts' => [
@@ -12,6 +12,7 @@ return [
             'enabled' => true,
         ],
 
+        // Base model class to be used for MorphTo relation return type
         'base_model' => \Illuminate\Database\Eloquent\Model::class,
     ],
 
@@ -21,7 +22,8 @@ return [
     ],
 
     'scopes' => [
-        // Enable generating model query scope methods
+        // Enable generating model query scope methods. Only looks for existing method prefixed with "scope"
+        // @method static \Illuminate\Database\Eloquent\Builder whereId(int $id)',
         'enabled' => true,
         // Define certain scope methods that should be ignored (provide final method name without "scope" prefix)
         'ignore' => [
