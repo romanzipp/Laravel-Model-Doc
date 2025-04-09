5cd01f08c0e7793b6ab31b12af282cd4e9ac8571
ToonDG <degrootetoon@gmail.com>
2025-04-09 16:17:18 +0200

    Normalize line endings in DocumentationGenerator and TestCase to use "\n"

--- src/Services/DocumentationGenerator.php
+++ src/Services/DocumentationGenerator.php
@@ -485,7 +485,7 @@ class DocumentationGenerator
 
         $lineIndexClassDeclaration = null;
 
-        $lines = explode(PHP_EOL, $content);
+        $lines = explode("\n", $content);
 
         foreach ($lines as $index => $line) {
             if ( ! preg_match('/^(abstract|final)? ?class ([A-z]+)/', $line)) {
@@ -514,7 +514,7 @@ class DocumentationGenerator
             $lines[$index] = null;
         }
 
-        $docLines = explode(PHP_EOL, $docblock->toString());
+        $docLines = explode("\n", $docblock->toString());
 
         foreach (array_reverse($docLines) as $docLine) {
             array_splice($lines, $lineIndexClassDeclaration, 0, $docLine);
@@ -522,7 +522,7 @@ class DocumentationGenerator
 
         $lines = array_filter($lines, static fn ($line) => null !== $line);
 
-        file_put_contents($reflectionClass->getFileName(), implode(PHP_EOL, $lines));
+        file_put_contents($reflectionClass->getFileName(), implode("\n", $lines));
     }
 
     /**
